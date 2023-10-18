77c2f06b7d325fb9783bf9e7e6f6cc9f33498291
Shariar Islam <shariarn85@gmail.com>
2023-10-18 19:27:53 +0600

    partially product update configured

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -23,37 +23,86 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const brands = [
+  {
+      "id": 1,
+      "brandName": "Toyota",
+      "brandImage": "https://i.ibb.co/pdd2v1q/toyota.png"
+  },
+  {
+      "id": 2,
+      "brandName": "Ford",
+      "brandImage": "https://i.ibb.co/1dZxwxR/Ford.jpg"
+  },
+  {
+      "id": 3,
+      "brandName": "BMW",
+      "brandImage": "https://i.ibb.co/hC6KX9j/BMW.jpg"
+  },
+  {
+      "id": 4,
+      "brandName": "Mercedes-Benz",
+      "brandImage": "https://i.ibb.co/VwyS5vt/Mercedes-Benz.jpg"
+  },
+  {
+      "id": 5,
+      "brandName": "Tesla",
+      "brandImage": "https://i.ibb.co/GPxqPGR/Tesla.jpg"
+  },
+  {
+      "id": 6,
+      "brandName": "Honda",
+      "brandImage": "https://i.ibb.co/9hCtpbr/Honda.png"
+  }
+]
+
+const productsCollection = client.db('vehiVerseDB').collection('products')
+const brandCollection = client.db('vehiVerseDB').collection('brand')
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+      // await client.connect();
 
 
-      const productsCollection = client.db('vehiVerseDB').collection('products')
+  
 
-      app.get('/products', async (req, res) => {
+    app.get('/products', async (req, res) => {
+      const body = req.body
+      console.log(body);
           
           const result = await productsCollection.find().toArray()
           res.send(result)
-      })
+    })
+    
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollection.findOne(query)
+      res.send(result)
+    })
 
-      app.get('/products/:brand', async (req, res) => {
-          const brand = req.body
-          
-          const query = {brand: brand}
-          const result = await productsCollection.findOne(query)
-          res.send(result)
-          console.log(result);
-
-      })
-
+  
       app.post('/products', async (req, res) => {
-          const products = req.body;
-          console.log(products);
-          const result = await productsCollection.insertOne(products)
+          const newProduct = req.body;
+          console.log(newProduct);
+          const result = await productsCollection.insertOne(newProduct)
           console.log(result);
           res.send(result)
       })
+    
+    
+    
+    
+    
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productsCollection.deleteOne(query)
+      res.send(result)
+
+    })
       
 
 
@@ -78,8 +127,11 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
+ 
     res.send('VehiVerse server side is running')
 })
+
+
 
 app.listen(port, () => {
     console.log(`VehiVerse server is running on port ${port}`);
